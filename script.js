3ff757874275d621b68935235ed2dff3307cf32a
mathos <bretmathilde@orange.fr>
2022-02-03 00:05:05 +0100

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+  } else {
+    document.querySelector("nav").style.top = "-100%";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 /* Faire apparaitre le panier */ 
 
 let panier = document.getElementById("cart");
